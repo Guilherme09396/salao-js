fec9e0ddf9c1b0249d563671eb0d8628bfe462c4
Guilherme Gomes Alves <guilherme09396@gmail.com>
2022-11-18 10:12:29 -0300

    primeiro commit

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAsCRpZpbYQQ7GDVOcw6x9JkYg86lnBKZM",
+  authDomain: "salao-jss.firebaseapp.com",
+  projectId: "salao-jss",
+  storageBucket: "salao-jss.appspot.com",
+  messagingSenderId: "200911542059",
+  appId: "1:200911542059:web:6c99ee7d70c96b5281176e",
+  measurementId: "G-CD6QJLX286"
+};
+
+module.exports = firebaseConfig;
